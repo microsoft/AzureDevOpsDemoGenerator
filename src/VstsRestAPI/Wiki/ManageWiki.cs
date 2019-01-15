1212e38d3450a500331d6b2daeb9a111ab5dbe37
Akshay H <h.akshay@outlook.com>
2019-01-15 09:04:06 +0530

    added project wiki

--- src/VstsRestAPI/Wiki/ManageWiki.cs
+++ src/VstsRestAPI/Wiki/ManageWiki.cs
@@ -26,7 +26,7 @@ namespace VstsRestAPI.Wiki
                     var jsonContent = new StringContent(jsonString, Encoding.UTF8, "application/json");
 
                     var method = new HttpMethod("POST");
-                    var request = new HttpRequestMessage(method, "/_apis/wiki/wikis?api-version=" + _configuration.VersionNumber) { Content = jsonContent };
+                    var request = new HttpRequestMessage(method, _configuration.UriString + "/_apis/wiki/wikis?api-version=" + _configuration.VersionNumber) { Content = jsonContent };
                     HttpResponseMessage response = client.SendAsync(request).Result;
                     if (response.IsSuccessStatusCode)
                     {
@@ -57,16 +57,17 @@ namespace VstsRestAPI.Wiki
         /// <param name="WikiId"></param>
         /// <param name="path"></param>
         /// <returns></returns>
-        public bool CreatePages(string jsonString, string projectName, string WikiId, string path)
+        public bool CreateUpdatePages(string jsonString, string projectName, string WikiId, string path)
         {
             try
             {
+                //PUT https://dev.azure.com/{organization}/{project}/_apis/wiki/wikis/{wikiIdentifier}/pages?path={path}&api-version=4.1
                 using (var client = GetHttpClient())
                 {
                     var jsonContent = new StringContent(jsonString, Encoding.UTF8, "application/json");
 
                     var method = new HttpMethod("PUT");
-                    var request = new HttpRequestMessage(method, projectName + "/_apis/wiki/wikis/" + WikiId + "/pages?path=/" + path + "&api-version=" + _configuration.VersionNumber) { Content = jsonContent };
+                    var request = new HttpRequestMessage(method, projectName + "/_apis/wiki/wikis/" + WikiId + "/pages?path=" + path + "&api-version=" + _configuration.VersionNumber) { Content = jsonContent };
                     HttpResponseMessage response = client.SendAsync(request).Result;
                     if (response.IsSuccessStatusCode)
                     {
@@ -88,5 +89,68 @@ namespace VstsRestAPI.Wiki
             }
             return false;
         }
+
+        public bool MovePages(string jsonString, string projectName, string WikiId)
+        {
+            try
+            {
+                using (var client = GetHttpClient())
+                {
+                    var jsonContent = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                    var method = new HttpMethod("POST");
+                    var request = new HttpRequestMessage(method, projectName + "/_apis/wiki/wikis/" + WikiId + "/pagemoves?&api-version=" + _configuration.VersionNumber) { Content = jsonContent };
+                    HttpResponseMessage response = client.SendAsync(request).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string result = response.Content.ReadAsStringAsync().Result;
+                        return true;
+                    }
+                    else
+                    {
+                        var errorMessage = response.Content.ReadAsStringAsync();
+                        string error = Utility.GeterroMessage(errorMessage.Result.ToString());
+                        this.LastFailureMessage = error;
+                        return false;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return false;
+        }
+
+        public bool DeletePage(string projectName, string WikiId, string path)
+        {
+            try
+            {
+                using (var client = GetHttpClient())
+                {
+                    var method = new HttpMethod("DELETE");
+                    var request = new HttpRequestMessage(method, projectName + "/_apis/wiki/wikis/" + WikiId + "/pages?path=" + path + "&api-version=" + _configuration.VersionNumber);
+                    HttpResponseMessage response = client.SendAsync(request).Result;
+                    if (response.IsSuccessStatusCode && response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        string result = response.Content.ReadAsStringAsync().Result;
+                        return true;
+                    }
+                    else
+                    {
+                        var errorMessage = response.Content.ReadAsStringAsync();
+                        string error = Utility.GeterroMessage(errorMessage.Result.ToString());
+                        this.LastFailureMessage = error;
+                        return false;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return false;
+        }
+
     }
 }
