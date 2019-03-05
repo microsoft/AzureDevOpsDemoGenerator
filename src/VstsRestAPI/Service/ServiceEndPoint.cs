d1df56a204c9cf8b6e67f6851e33fbff355eff61
Akshay H <h.akshay@outlook.com>
2019-03-05 12:21:52 +0530

    fectching endpoints

--- src/VstsRestAPI/Service/ServiceEndPoint.cs
+++ src/VstsRestAPI/Service/ServiceEndPoint.cs
@@ -1,5 +1,7 @@
-﻿using System.Net.Http;
+﻿using Newtonsoft.Json;
+using System.Net.Http;
 using System.Text;
+using VstsRestAPI.Viewmodel.Extractor;
 using VstsRestAPI.Viewmodel.Service;
 
 namespace VstsRestAPI.Service
@@ -28,7 +30,7 @@ namespace VstsRestAPI.Service
 
                 if (response.IsSuccessStatusCode)
                 {
-                    viewModel = response.Content.ReadAsAsync<ServiceEndpointModel>().Result;                    
+                    viewModel = response.Content.ReadAsAsync<ServiceEndpointModel>().Result;
                 }
                 else
                 {
@@ -39,7 +41,31 @@ namespace VstsRestAPI.Service
             }
 
             return viewModel;
-            
+
+        }
+
+        public GetServiceEndpoints.ServiceEndPoint GetServiceEndPoints()
+        {
+            //https://dev.azure.com/exakshay/endpoint/_apis/serviceendpoint/endpoints?api-version=4.1-preview.1
+            using (var client = GetHttpClient())
+            {
+                var request = string.Format("{0}{1}/_apis/serviceendpoint/endpoints?api-version={2}", _configuration.UriString, Project, _configuration.VersionNumber);
+                HttpResponseMessage response = client.GetAsync(request).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string res = response.Content.ReadAsStringAsync().Result;
+                    GetServiceEndpoints.ServiceEndPoint serviceEndPoint = JsonConvert.DeserializeObject<GetServiceEndpoints.ServiceEndPoint>(res);
+                    return serviceEndPoint;
+                }
+                else
+                {
+                    var errorMessage = response.Content.ReadAsStringAsync();
+                    string error = Utility.GeterroMessage(errorMessage.Result.ToString());
+                    this.LastFailureMessage = error;
+                    return new GetServiceEndpoints.ServiceEndPoint();
+
+                }
+            }
         }
     }
 }
