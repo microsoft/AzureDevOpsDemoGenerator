cfe076d388ddc3651c6df0b7f4e3e89560218b68
manjunatha.navalagunda <manjunatha.n@ecanarys.com>
2019-04-23 11:15:43 +0530

    Added dependecy injection with unity

--- src/VstsDemoBuilder/ServiceInterfaces/IProjectService.cs
+++ src/VstsDemoBuilder/ServiceInterfaces/IProjectService.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace VstsDemoBuilder.ServiceInterfaces
 {
-    interface IProjectService
+    public interface IProjectService
     {
+        string GetMessage();
     }
 }
--- src/VstsDemoBuilder/Services/ProjectService.cs
+++ src/VstsDemoBuilder/Services/ProjectService.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using VstsDemoBuilder.ServiceInterfaces;
 
 namespace VstsDemoBuilder.Services
 {
-    public class ProjectService
+    public class ProjectService : IProjectService
     {
+        public string GetMessage()
+        {
+            return "Welcome";
+        }
     }
 }
