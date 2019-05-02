bdd41f5196f8ef4c64ed06b36ab8a7076a2f17e3
manjunatha.navalagunda <manjunatha.n@ecanarys.com>
2019-05-02 18:26:22 +0530

    Added swash buckle from nuget package manager

--- src/VstsDemoBuilder/App_Start/WebApiConfig.cs
+++ src/VstsDemoBuilder/App_Start/WebApiConfig.cs
@@ -9,11 +9,11 @@ namespace VstsDemoBuilder
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-              name: "ActionApi",
-              routeTemplate: "api/{controller}/{Action}/{id}",
-              defaults: new { id = RouteParameter.Optional }
-          );
+          //  config.Routes.MapHttpRoute(
+          //    name: "ActionApi",
+          //    routeTemplate: "api/{controller}/{Action}/{id}",
+          //    defaults: new { id = RouteParameter.Optional }
+          //);
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
