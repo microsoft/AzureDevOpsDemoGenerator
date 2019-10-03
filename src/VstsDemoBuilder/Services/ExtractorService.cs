da64c8e89ae8da4d0b44524662ebebbbb8ad0ddc
Akshay Hosur <akshay.hosur@ecanarys.com>
2019-10-03 16:47:20 +0530

    removing old templates which are not downloded from past 1 hour

--- src/VstsDemoBuilder/Services/ExtractorService.cs
+++ src/VstsDemoBuilder/Services/ExtractorService.cs
@@ -197,6 +197,20 @@ namespace VstsDemoBuilder.Services
         public string[] GenerateTemplateArifacts(Project model)
         {
             extractedTemplatePath = HostingEnvironment.MapPath("~") + @"ExtractedTemplate\";
+
+            if (Directory.Exists(extractedTemplatePath))
+            {
+                string[] subdirs = Directory.GetDirectories(extractedTemplatePath)
+                               .Select(Path.GetFileName)
+                               .ToArray();
+                foreach (string folderName in subdirs)
+                {
+                    DirectoryInfo d = new DirectoryInfo(extractedTemplatePath + folderName);
+                    if (d.CreationTime < DateTime.Now.AddHours(-1))
+                        Directory.Delete(extractedTemplatePath + folderName, true);
+                }
+            }
+
             AddMessage(model.id, "");
             ProjectConfigurations appConfig = ProjectConfiguration(model);
 
