2ec76379852e764a390e35b7733d006bf11be40a
isaac2004 <isaac.r.levin@gmail.com>
2019-10-25 10:49:44 -0400

    Port to .NET Core 3.0

---
+++ src/AzureDevOpsDemoBuilder/Controllers/AccountController.cs
@@ -0,0 +1,174 @@
+﻿using log4net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using VstsDemoBuilder.Models;
+using VstsDemoBuilder.ServiceInterfaces;
+
+namespace VstsDemoBuilder.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        private readonly AccessDetails accessDetails = new AccessDetails();
+        private TemplateSelection.Templates templates = new TemplateSelection.Templates();
+        private ILog logger = LogManager.GetLogger(typeof(AccountController));
+        private IProjectService projectService;
+        private  IWebHostEnvironment HostingEnvironment;
+        private ISession Session;
+
+        public AccountController(IProjectService _projectService, IWebHostEnvironment _host, ISession _session)
+        {
+            Session = _session;
+            projectService = _projectService;
+            HostingEnvironment = _host;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Unsupported_browser()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Verify View
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Verify(LoginModel model, string id)
+        {
+            Session.Clear();
+            // check to enable extractor
+            if (string.IsNullOrEmpty(model.EnableExtractor) || model.EnableExtractor.ToLower() == "false")
+            {
+                model.EnableExtractor = System.Configuration.ConfigurationManager.AppSettings["EnableExtractor"];
+            }
+            if (!string.IsNullOrEmpty(model.EnableExtractor))
+            {
+                HttpContext.Session.SetString("EnableExtractor", model.EnableExtractor);
+            }
+            var browser = Request.Headers["User-Agent"].ToString();
+            if (browser.Contains("InternetExplorer"))
+            {
+                return RedirectToAction("Unsupported_browser", "Account");
+            }
+            try
+            {
+                if (!string.IsNullOrEmpty(model.name))
+                {
+                    if (System.IO.File.Exists(HostingEnvironment.WebRootPath + @"\Templates\TemplateSetting.json"))
+                    {
+                        string privateTemplatesJson = System.IO.File.ReadAllText(HostingEnvironment.WebRootPath + @"\Templates\TemplateSetting.json");
+                        templates = Newtonsoft.Json.JsonConvert.DeserializeObject<TemplateSelection.Templates>(privateTemplatesJson);
+                        if (templates != null)
+                        {
+                            bool flag = false;
+                            foreach (var grpTemplate in templates.GroupwiseTemplates)
+                            {
+                                foreach (var template in grpTemplate.Template)
+                                {
+                                    if (template.ShortName != null && template.ShortName.ToLower() == model.name.ToLower())
+                                    {
+                                        flag = true;
+                                        HttpContext.Session.SetString("templateName", template.Name);
+                                    }
+                                }
+                            }
+                            if (flag == false)
+                            {
+                                HttpContext.Session.SetString("templateName", null);
+                            }
+                        }
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(model.Event))
+                {
+                    string eventsTemplate = HostingEnvironment.WebRootPath + @"\Templates\Events.json";
+                    if (System.IO.File.Exists(eventsTemplate))
+                    {
+                        string eventContent = System.IO.File.ReadAllText(eventsTemplate);
+                        var jItems = JObject.Parse(eventContent);
+                        if (jItems[model.Event] != null)
+                        {
+                            model.Event = jItems[model.Event].ToString();
+                        }
+                        else
+                        {
+                            model.Event = string.Empty;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Debug(JsonConvert.SerializeObject(ex, Formatting.Indented) + Environment.NewLine);
+            }
+            return View(model);
+        }
+
+        /// <summary>
+        /// Get Account at the end of project provision
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public string GetAccountName()
+        {            
+            if (HttpContext.Session.GetString("AccountName") != null)
+            {
+                string accountName = HttpContext.Session.GetString("AccountName").ToString();
+                return accountName;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Index view which calls VSTS OAuth
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            try
+            {
+                HttpContext.Session.SetString("visited", "1");
+                string url = "https://app.vssps.visualstudio.com/oauth2/authorize?client_id={0}&response_type=Assertion&state=User1&scope={1}&redirect_uri={2}";
+                string redirectUrl = System.Configuration.ConfigurationManager.AppSettings["RedirectUri"];
+                string clientId = System.Configuration.ConfigurationManager.AppSettings["ClientId"];
+                string AppScope = System.Configuration.ConfigurationManager.AppSettings["appScope"];
+                url = string.Format(url, clientId, AppScope, redirectUrl);
+                return Redirect(url);
+            }
+            catch (Exception ex)
+            {
+                logger.Debug(JsonConvert.SerializeObject(ex, Formatting.Indented) + Environment.NewLine);
+            }
+            return RedirectToAction("../shared/error");
+        }
+
+        /// <summary>
+        /// Sign out
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult SignOut()
+        {
+            Session.Clear();
+            return Redirect("https://app.vssps.visualstudio.com/_signout");
+        }
+    }
+}
--- src/VstsDemoBuilder/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VstsDemoBuilder.MvcApplication" Language="C#" %>
