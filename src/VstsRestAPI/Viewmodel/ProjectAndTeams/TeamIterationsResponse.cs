c3b7a83b79fe57a1bf3c1d16ae36a0c68cf06ab5
Akshay H <38463158+aksh-h@users.noreply.github.com>
2020-03-13 11:40:22 +0530

    Aksh/team iteration  AB#3225 #3321 #3326 (#209)
    * adding iterations to teams and managing sprint dates
    
    * added comment
    
    * adding delivery plans template
    
    * checking for iteraration map json to map the itearations to teams
    
    * checking for file existance
    
    * corrected delivery plans template and
    
    * changed Iteartion fetch API and corrected related logic
    
    * removed users and added tempalte information
    
    * added spacegame-azurefunction tempalte
    
    * corrected work item title and description
    
    Co-authored-by: Akshay H <aksh-h@users.noreply.github.com>

--- src/VstsRestAPI/Viewmodel/ProjectAndTeams/TeamIterationsResponse.cs
+++ src/VstsRestAPI/Viewmodel/ProjectAndTeams/TeamIterationsResponse.cs
@@ -8,11 +8,23 @@ namespace VstsRestAPI.Viewmodel.ProjectAndTeams
 {
     public class TeamIterationsResponse
     {
+        public class Child
+        {
+            public string identifier { get; set; }
+            public string name { get; set; }
+            public string structureType { get; set; }
+            public bool hasChildren { get; set; }
+            public string path { get; set; }
+        }
+
         public class Value
         {
-            public string id { get; set; }
+            public string identifier { get; set; }
             public string name { get; set; }
-
+            public string structureType { get; set; }
+            public bool hasChildren { get; set; }
+            public string path { get; set; }
+            public List<Child> children { get; set; }
         }
 
         public class Iterations
@@ -20,5 +32,17 @@ namespace VstsRestAPI.Viewmodel.ProjectAndTeams
             public int count { get; set; }
             public IList<Value> value { get; set; }
         }
+        //public class Value
+        //{
+        //    public string id { get; set; }
+        //    public string name { get; set; }
+
+        //}
+
+        //public class Iterations
+        //{
+        //    public int count { get; set; }
+        //    public IList<Value> value { get; set; }
+        //}
     }
 }
