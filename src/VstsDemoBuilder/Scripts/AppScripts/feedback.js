e2795928960508ea246a8a5a2bc101fc0ae93d8a
aksh-h <akshay.hosur@ecanarys.com>
2022-09-22 15:03:54 +0530

    feedback updated

--- src/VstsDemoBuilder/Scripts/AppScripts/feedback.js
+++ src/VstsDemoBuilder/Scripts/AppScripts/feedback.js
@@ -7,12 +7,17 @@
 
         var noofyears = $("input[type='radio'][name='noofyears']:checked").val();
 
+        // come to know
         var blog = $("input[type='checkbox'][name='blog']:checked").val();
         var social = $("input[type='checkbox'][name='social']:checked").val();
         var Emails = $("input[type='checkbox'][name='Emails']:checked").val();
         var friendscolleagues = $("input[type='checkbox'][name='friendscolleagues']:checked").val();
         var other = $("input[type='checkbox'][name='other']:checked").val();
 
+        // purpose
+        var noofyears = $("input[type='radio'][name='noofyears']:checked").val();
+
+        // template section
         var general = $("input[type='checkbox'][name='general']:checked").val();
         var devopslabs = $("input[type='checkbox'][name='devopslabs']:checked").val();
         var mslearn = $("input[type='checkbox'][name='mslearn']:checked").val();
@@ -21,8 +26,13 @@
         var FastTrackonAzure = $("input[type='checkbox'][name='FastTrackonAzure']:checked").val();
         var private = $("input[type='checkbox'][name='private']:checked").val();
 
+        // most user template
         var usedtemplatenames = $('#usedtemplatenames').val();
+
+        // kind of template
         var kindoftemplates = $('#kindoftemplates').val();
+
+        // other feedback
         var otherfeedback = $('#otherfeedback').val();
 
         var state = true;
